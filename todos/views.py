0422a90fc11fc379c4916d5af43786c45a9a8eb9
Randy <anakkantorku@gmail.com>
2025-03-24 05:18:43 +0700

    Create todo Variable in views.py

--- todos/views.py
+++ todos/views.py
@@ -7,5 +7,6 @@ def index_view(request):
     return render(request, "index.html", {'todos': todos})
 
 
-def detail_view(request):
-    return render(request, "detail.html" {})
+def detail_view(request, id):
+    todo = Todo.objects.get(id=id)
+    return render(request, "detail.html", {'todo':todo})
