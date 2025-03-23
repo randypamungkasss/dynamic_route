cec3955b912bc0ea9ecda03a61ba6047a2a7d1f3
Randy <anakkantorku@gmail.com>
2025-03-24 04:32:10 +0700

    Create model in /todos/models.py

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextFieldField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
